cc1b71483b7a21f854cbae2a00ea5f52d9e76fa0
julio-silveira <julioqueiroz15@gmail.com>
2022-12-08 14:15:08 -0300

    chore: started changing db from  mysql to postgres

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,12 +2,12 @@ import 'dotenv/config'
 import { Options } from 'sequelize'
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
-  database: process.env.DB_NAME || 'crud_db',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql'
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || '123456',
+  database: process.env.DB_NAME || 'NG.CASH_DB',
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT) || 5432,
+  dialect: 'postgres'
 }
 
 export = config
