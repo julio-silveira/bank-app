4f9ad38b017f322c5881477b41485a076f936fa8
julio-silveira <julioqueiroz15@gmail.com>
2022-12-10 21:17:41 -0300

    feat: transaction body middleware

--- app/backend/src/controllers/transactions..controller.ts
+++ app/backend/src/controllers/transactions..controller.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express'
 import statusCodes from '../statusCodes'
 import TransactionServices from '../services/transactions.service'
-import sequelize, { Transaction } from 'sequelize'
 
 export default class TaskControler {
   constructor(private transactionServices = new TransactionServices()) {}
 
-  public getTasks = async (req: Request, res: Response) => {
+  public getAll = async (req: Request, res: Response) => {
     const accountId = Number(req.params.accountId)
     const transactions = await this.transactionServices.findAll(accountId)
     res.status(statusCodes.OK).json(transactions)
@@ -22,21 +21,6 @@ export default class TaskControler {
     })
     res
       .status(statusCodes.CREATED)
-      .json({ message: 'Tarefa criada com sucesso!' })
+      .json({ message: 'Transação efetuada com sucesso!' })
   }
-
-  // public update = async (req: Request, res: Response) => {
-  //   const userId = Number(req.params.userId)
-  //   const taskId = Number(req.params.taskId)
-  //   const tasksData = req.body
-  //   await this.taskServices.update(userId, taskId, { userId, ...tasksData })
-  //   res.status(statusCodes.OK).json({ message: 'Tarefa editada com sucesso' })
-  // }
-
-  // public remove = async (req: Request, res: Response) => {
-  //   const userId = Number(req.params.userId)
-  //   const taskId = Number(req.params.taskId)
-  //   await this.taskServices.remove(userId, taskId)
-  //   res.status(statusCodes.OK).json({ message: 'Tarefa excluída com sucesso' })
-  // }
 }
--- app/backend/src/routes/transactions.routes.ts
+++ app/backend/src/routes/transactions.routes.ts
@@ -1,20 +1,25 @@
 import { Router } from 'express'
-import TaskControler from '../controllers/transactions..controller'
+import TransactionController from '../controllers/transactions..controller'
 import ValidateJWT from '../auth/validateJWT'
 
 const TRANSACTION_ROUTE = '/transactions/:accountId'
-const ONE_TASK_ROUTE = '/users/:userId/tasks/:taskId'
 
 const router = Router()
 
 const validateJWT = new ValidateJWT()
 
-const taskController = new TaskControler()
+const transactionController = new TransactionController()
 
-router.get(TRANSACTION_ROUTE, validateJWT.tokenAuth, taskController.getTasks)
-// router.get(ONE_TASK_ROUTE, validateJWT.tokenAuth, taskController.getTask)
-// router.post(ALL_TASKS_ROUTE, validateJWT.tokenAuth, taskController.create)
-// router.put(ONE_TASK_ROUTE, validateJWT.tokenAuth, taskController.update)
-// router.delete(ONE_TASK_ROUTE, validateJWT.tokenAuth, taskController.remove)
+router.get(
+  TRANSACTION_ROUTE,
+  validateJWT.tokenAuth,
+  transactionController.getAll
+)
+
+router.post(
+  TRANSACTION_ROUTE,
+  validateJWT.tokenAuth,
+  transactionController.create
+)
 
 export default router
